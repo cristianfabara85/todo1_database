853a72f7a7ba29591b6066f6ea45fb2414af3986
cfabara <101440161+cristianfabara85@users.noreply.github.com>
2022-03-12 23:43:50 -0500

    Adicion de tabla de usuarios

--- backup_demo.sql
+++ backup_demo.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 12.2
 -- Dumped by pg_dump version 12.2
 
--- Started on 2022-03-12 21:22:36
+-- Started on 2022-03-12 23:41:03
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -20,7 +20,7 @@ SET row_security = off;
 
 DROP DATABASE demo;
 --
--- TOC entry 2867 (class 1262 OID 59947)
+-- TOC entry 2879 (class 1262 OID 59947)
 -- Name: demo; Type: DATABASE; Schema: -; Owner: postgres
 --
 
@@ -41,6 +41,8 @@ SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
+
+SET default_tablespace = '';
 
 SET default_table_access_method = heap;
 
@@ -76,7 +78,7 @@ CREATE SEQUENCE public.catalogos_catalogoid_seq
 ALTER TABLE public.catalogos_catalogoid_seq OWNER TO postgres;
 
 --
--- TOC entry 2868 (class 0 OID 0)
+-- TOC entry 2880 (class 0 OID 0)
 -- Dependencies: 204
 -- Name: catalogos_catalogoid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -119,7 +121,7 @@ CREATE SEQUENCE public.compras_compraid_seq
 ALTER TABLE public.compras_compraid_seq OWNER TO postgres;
 
 --
--- TOC entry 2869 (class 0 OID 0)
+-- TOC entry 2881 (class 0 OID 0)
 -- Dependencies: 206
 -- Name: compras_compraid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -159,7 +161,7 @@ CREATE SEQUENCE public.productos_productoid_seq
 ALTER TABLE public.productos_productoid_seq OWNER TO postgres;
 
 --
--- TOC entry 2870 (class 0 OID 0)
+-- TOC entry 2882 (class 0 OID 0)
 -- Dependencies: 202
 -- Name: productos_productoid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -200,12 +202,54 @@ CREATE SEQUENCE public.stock_stockid_seq
 ALTER TABLE public.stock_stockid_seq OWNER TO postgres;
 
 --
--- TOC entry 2871 (class 0 OID 0)
+-- TOC entry 2883 (class 0 OID 0)
 -- Dependencies: 210
 -- Name: stock_stockid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
 ALTER SEQUENCE public.stock_stockid_seq OWNED BY public.stock.stockid;
+
+
+--
+-- TOC entry 213 (class 1259 OID 68173)
+-- Name: usuarios; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.usuarios (
+    usuarioid integer NOT NULL,
+    nombre character varying(100) NOT NULL,
+    correo character varying(100),
+    usuario character varying(100) NOT NULL,
+    password character varying(300) NOT NULL,
+    tipo integer NOT NULL
+);
+
+
+ALTER TABLE public.usuarios OWNER TO postgres;
+
+--
+-- TOC entry 212 (class 1259 OID 68171)
+-- Name: usuarios_usuarioid_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.usuarios_usuarioid_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.usuarios_usuarioid_seq OWNER TO postgres;
+
+--
+-- TOC entry 2884 (class 0 OID 0)
+-- Dependencies: 212
+-- Name: usuarios_usuarioid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.usuarios_usuarioid_seq OWNED BY public.usuarios.usuarioid;
 
 
 --
@@ -243,7 +287,7 @@ CREATE SEQUENCE public.ventas_ventaid_seq
 ALTER TABLE public.ventas_ventaid_seq OWNER TO postgres;
 
 --
--- TOC entry 2872 (class 0 OID 0)
+-- TOC entry 2885 (class 0 OID 0)
 -- Dependencies: 208
 -- Name: ventas_ventaid_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -252,7 +296,7 @@ ALTER SEQUENCE public.ventas_ventaid_seq OWNED BY public.ventas.ventaid;
 
 
 --
--- TOC entry 2712 (class 2604 OID 60026)
+-- TOC entry 2719 (class 2604 OID 60026)
 -- Name: catalogos catalogoid; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -260,7 +304,7 @@ ALTER TABLE ONLY public.catalogos ALTER COLUMN catalogoid SET DEFAULT nextval('p
 
 
 --
--- TOC entry 2713 (class 2604 OID 68144)
+-- TOC entry 2720 (class 2604 OID 68144)
 -- Name: compras compraid; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -268,7 +312,7 @@ ALTER TABLE ONLY public.compras ALTER COLUMN compraid SET DEFAULT nextval('publi
 
 
 --
--- TOC entry 2711 (class 2604 OID 59972)
+-- TOC entry 2718 (class 2604 OID 59972)
 -- Name: productos productoid; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -276,7 +320,7 @@ ALTER TABLE ONLY public.productos ALTER COLUMN productoid SET DEFAULT nextval('p
 
 
 --
--- TOC entry 2715 (class 2604 OID 68168)
+-- TOC entry 2722 (class 2604 OID 68168)
 -- Name: stock stockid; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -284,7 +328,15 @@ ALTER TABLE ONLY public.stock ALTER COLUMN stockid SET DEFAULT nextval('public.s
 
 
 --
--- TOC entry 2714 (class 2604 OID 68152)
+-- TOC entry 2723 (class 2604 OID 68176)
+-- Name: usuarios usuarioid; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.usuarios ALTER COLUMN usuarioid SET DEFAULT nextval('public.usuarios_usuarioid_seq'::regclass);
+
+
+--
+-- TOC entry 2721 (class 2604 OID 68152)
 -- Name: ventas ventaid; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -292,7 +344,7 @@ ALTER TABLE ONLY public.ventas ALTER COLUMN ventaid SET DEFAULT nextval('public.
 
 
 --
--- TOC entry 2855 (class 0 OID 60023)
+-- TOC entry 2865 (class 0 OID 60023)
 -- Dependencies: 205
 -- Data for Name: catalogos; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -306,15 +358,18 @@ INSERT INTO public.catalogos (catalogoid, tipo, nombre, estado) VALUES (12, 2, '
 
 
 --
--- TOC entry 2857 (class 0 OID 68141)
+-- TOC entry 2867 (class 0 OID 68141)
 -- Dependencies: 207
 -- Data for Name: compras; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
+INSERT INTO public.compras (compraid, productoid, proveedor, cantidad, fecharegistro, preciounitario, preciototal) VALUES (5, '3', 'Corporacion Favorita', 12, '2022-03-12', 5.00, 60.00);
+INSERT INTO public.compras (compraid, productoid, proveedor, cantidad, fecharegistro, preciounitario, preciototal) VALUES (6, '5', 'Comics Store S.A', 3, '2022-03-12', 25.50, 76.50);
+INSERT INTO public.compras (compraid, productoid, proveedor, cantidad, fecharegistro, preciounitario, preciototal) VALUES (7, '3', 'Comics Store S.A', 24, '2022-03-12', 5.75, 138.00);
 
 
 --
--- TOC entry 2853 (class 0 OID 59969)
+-- TOC entry 2863 (class 0 OID 59969)
 -- Dependencies: 203
 -- Data for Name: productos; Type: TABLE DATA; Schema: public; Owner: postgres
 --
@@ -327,34 +382,48 @@ INSERT INTO public.productos (productoid, codigo, nombre, preciounidad) VALUES (
 INSERT INTO public.productos (productoid, codigo, nombre, preciounidad) VALUES (9, 'vb08', 'Camiseta Libre Soy', 9.00);
 INSERT INTO public.productos (productoid, codigo, nombre, preciounidad) VALUES (10, 'vb09', 'Vaso Mario Bross 250 ml', 2.50);
 INSERT INTO public.productos (productoid, codigo, nombre, preciounidad) VALUES (11, 'vb10', 'Lampara Mario Bross', 4.50);
+INSERT INTO public.productos (productoid, codigo, nombre, preciounidad) VALUES (12, 'vb11', 'Jarro Iron Man', 17.00);
 
 
 --
--- TOC entry 2861 (class 0 OID 68165)
+-- TOC entry 2871 (class 0 OID 68165)
 -- Dependencies: 211
 -- Data for Name: stock; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
 INSERT INTO public.stock (stockid, productoid, entradas, salidas, disponible) VALUES (9, 6, 0, 0, 0);
 INSERT INTO public.stock (stockid, productoid, entradas, salidas, disponible) VALUES (10, 2, 0, 0, 0);
-INSERT INTO public.stock (stockid, productoid, entradas, salidas, disponible) VALUES (11, 3, 0, 0, 0);
-INSERT INTO public.stock (stockid, productoid, entradas, salidas, disponible) VALUES (12, 5, 0, 0, 0);
 INSERT INTO public.stock (stockid, productoid, entradas, salidas, disponible) VALUES (13, 8, 0, 0, 0);
 INSERT INTO public.stock (stockid, productoid, entradas, salidas, disponible) VALUES (14, 9, 0, 0, 0);
 INSERT INTO public.stock (stockid, productoid, entradas, salidas, disponible) VALUES (15, 10, 0, 0, 0);
 INSERT INTO public.stock (stockid, productoid, entradas, salidas, disponible) VALUES (16, 11, 0, 0, 0);
+INSERT INTO public.stock (stockid, productoid, entradas, salidas, disponible) VALUES (12, 5, 3, 0, 3);
+INSERT INTO public.stock (stockid, productoid, entradas, salidas, disponible) VALUES (11, 3, 36, 11, 25);
+INSERT INTO public.stock (stockid, productoid, entradas, salidas, disponible) VALUES (17, 12, 0, 0, 0);
 
 
 --
--- TOC entry 2859 (class 0 OID 68149)
+-- TOC entry 2873 (class 0 OID 68173)
+-- Dependencies: 213
+-- Data for Name: usuarios; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.usuarios (usuarioid, nombre, correo, usuario, password, tipo) VALUES (5, 'Christian Fabara', 'cristianfabara@hotmail.com', 'cristianfabara', 'TEZCHomQNZTTwmhTFqrEew==', 2);
+INSERT INTO public.usuarios (usuarioid, nombre, correo, usuario, password, tipo) VALUES (6, 'Viviana Santos', 'vsantos@hotmail.com', 'vsantos', 'TEZCHomQNZTTwmhTFqrEew==', 1);
+
+
+--
+-- TOC entry 2869 (class 0 OID 68149)
 -- Dependencies: 209
 -- Data for Name: ventas; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
+INSERT INTO public.ventas (ventaid, productoid, cliente, cantidad, fecharegistro, preciounitario, preciototal) VALUES (1, '3', 'Viviana Santos', 6, '2022-03-12', 8.00, 48.00);
+INSERT INTO public.ventas (ventaid, productoid, cliente, cantidad, fecharegistro, preciounitario, preciototal) VALUES (2, '3', 'Christian Fabara', 5, '2022-03-12', 12.83, 64.15);
 
 
 --
--- TOC entry 2873 (class 0 OID 0)
+-- TOC entry 2886 (class 0 OID 0)
 -- Dependencies: 204
 -- Name: catalogos_catalogoid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
@@ -363,43 +432,52 @@ SELECT pg_catalog.setval('public.catalogos_catalogoid_seq', 12, true);
 
 
 --
--- TOC entry 2874 (class 0 OID 0)
+-- TOC entry 2887 (class 0 OID 0)
 -- Dependencies: 206
 -- Name: compras_compraid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.compras_compraid_seq', 3, true);
+SELECT pg_catalog.setval('public.compras_compraid_seq', 7, true);
 
 
 --
--- TOC entry 2875 (class 0 OID 0)
+-- TOC entry 2888 (class 0 OID 0)
 -- Dependencies: 202
 -- Name: productos_productoid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.productos_productoid_seq', 11, true);
+SELECT pg_catalog.setval('public.productos_productoid_seq', 12, true);
 
 
 --
--- TOC entry 2876 (class 0 OID 0)
+-- TOC entry 2889 (class 0 OID 0)
 -- Dependencies: 210
 -- Name: stock_stockid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.stock_stockid_seq', 16, true);
+SELECT pg_catalog.setval('public.stock_stockid_seq', 17, true);
 
 
 --
--- TOC entry 2877 (class 0 OID 0)
+-- TOC entry 2890 (class 0 OID 0)
+-- Dependencies: 212
+-- Name: usuarios_usuarioid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.usuarios_usuarioid_seq', 6, true);
+
+
+--
+-- TOC entry 2891 (class 0 OID 0)
 -- Dependencies: 208
 -- Name: ventas_ventaid_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.ventas_ventaid_seq', 1, false);
+SELECT pg_catalog.setval('public.ventas_ventaid_seq', 2, true);
 
 
 --
--- TOC entry 2719 (class 2606 OID 60028)
+-- TOC entry 2727 (class 2606 OID 60028)
 -- Name: catalogos catalogoid_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -408,7 +486,7 @@ ALTER TABLE ONLY public.catalogos
 
 
 --
--- TOC entry 2721 (class 2606 OID 68146)
+-- TOC entry 2729 (class 2606 OID 68146)
 -- Name: compras compraid_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -417,7 +495,7 @@ ALTER TABLE ONLY public.compras
 
 
 --
--- TOC entry 2717 (class 2606 OID 59974)
+-- TOC entry 2725 (class 2606 OID 59974)
 -- Name: productos producto_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -426,7 +504,7 @@ ALTER TABLE ONLY public.productos
 
 
 --
--- TOC entry 2725 (class 2606 OID 68170)
+-- TOC entry 2733 (class 2606 OID 68170)
 -- Name: stock stockid_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -435,7 +513,16 @@ ALTER TABLE ONLY public.stock
 
 
 --
--- TOC entry 2723 (class 2606 OID 68154)
+-- TOC entry 2735 (class 2606 OID 68181)
+-- Name: usuarios usuarioid_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.usuarios
+    ADD CONSTRAINT usuarioid_pk PRIMARY KEY (usuarioid);
+
+
+--
+-- TOC entry 2731 (class 2606 OID 68154)
 -- Name: ventas ventaid_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -443,7 +530,7 @@ ALTER TABLE ONLY public.ventas
     ADD CONSTRAINT ventaid_pk PRIMARY KEY (ventaid);
 
 
--- Completed on 2022-03-12 21:22:37
+-- Completed on 2022-03-12 23:41:03
 
 --
 -- PostgreSQL database dump complete
